afa8016a70c33ca06a75212106eaca4bffcbc35c
Wagner Andrade <wagnerandrade_df@hotmail.com>
2025-02-20 12:13:47 -0300

    Teste

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -8,6 +8,7 @@ public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
+		//** TEste **// 
 	}
 
 }
