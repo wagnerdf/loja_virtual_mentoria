3e40ed65012282e37139c176dbe55cc0c98b52b7
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-02-23 12:57:09 -0300

    Implementação classes de model mentoria

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/PessoaJuridica.java
@@ -0,0 +1,60 @@
+package jdev.mentoria.lojavirtual.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pessoa_juridica")
+/*@PrimaryKeyJoinColumn(name = "id")*/
+public class PessoaJuridica extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String cnpj;
+	private String inscEstadual;
+	private String inscMunicipa;
+	private String nomeFantasia;
+	private String razaoSocial;
+	private String categoria;
+	
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getInscEstadual() {
+		return inscEstadual;
+	}
+	public void setInscEstadual(String inscEstadual) {
+		this.inscEstadual = inscEstadual;
+	}
+	public String getInscMunicipa() {
+		return inscMunicipa;
+	}
+	public void setInscMunicipa(String inscMunicipa) {
+		this.inscMunicipa = inscMunicipa;
+	}
+	public String getNomeFantasia() {
+		return nomeFantasia;
+	}
+	public void setNomeFantasia(String nomeFantasia) {
+		this.nomeFantasia = nomeFantasia;
+	}
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
