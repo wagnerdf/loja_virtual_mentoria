b995ed290a5e5b26c2e2fe50b9a35230a719b85c
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-02-25 18:44:35 -0300

    Criando enum tipo endereço e models Usuario e UsuarioAcesso.

--- src/main/java/jdev/mentoria/lojavirtual/model/Endereco.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Endereco.java
@@ -5,6 +5,8 @@ import java.util.Objects;
 
 import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,6 +15,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import jdev.mentoria.lojavirtual.enums.TipoEndereco;
 
 @Entity 
 @Table(name = "endereco")
@@ -35,7 +38,18 @@ public class Endereco implements Serializable {
 	@ManyToOne(targetEntity = Pessoa.class)
 	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
 	private Pessoa pessoa;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoEndereco tipoEndereco; 
 
+	public void setTipoEndereco(TipoEndereco tipoEndereco) {
+		this.tipoEndereco = tipoEndereco;
+	}
+	
+	public TipoEndereco getTipoEndereco() {
+		return tipoEndereco;
+	}
+	
 	public Long getId() {
 		return id;
 	}
