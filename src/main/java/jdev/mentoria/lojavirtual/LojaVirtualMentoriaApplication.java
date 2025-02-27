1755833538ddddd6a1d4290924e8bb93b8ff81f0
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-02-27 17:24:48 -0300

    Criação das models cupon desconto, imagem produto, nota fiscal compra, nota item produto, produto; Atualização de itens no enums; Anotação do @EntityScan no corpo do projeto.

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,13 +2,15 @@ package jdev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**import org.springframework.boot.autoconfigure.domain.EntityScan;**/
 
 @SpringBootApplication
+/**Anotação abaixo ativar somente se estiver com problemas de não criar as tabelas**/
+/**@EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")**/
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
-		//** TEste **// 
 	}
 
 }
