b8633f436271156e9699754c7854db9800a0fe35
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-03-06 15:58:16 -0300

    Adicionado SQL Completo do Banco de Dados ao projeto; Criação de pacotes Repository, Service e Controller; Configuração do application.properties; Anotas a classe Application do Spring Boot; Testes básicos de CRUD para as models.

--- src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -2,11 +2,16 @@ package jdev.mentoria.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/**import org.springframework.boot.autoconfigure.domain.EntityScan;**/
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-/**Anotação abaixo ativar somente se estiver com problemas de não criar as tabelas**/
-/**@EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")**/
+@EntityScan(basePackages = "jdev.mentoria.lojavirtual.model")
+@ComponentScan(basePackages = {"jdev.*"})
+@EnableJpaRepositories(basePackages = {"jdev.mentoria.lojavirtual.repository"})
+@EnableTransactionManagement
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -1,13 +1,35 @@
 package jdev.mentoria.lojavirtual;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import jdev.mentoria.lojavirtual.controller.AcessoController;
+import jdev.mentoria.lojavirtual.model.Acesso;
+import jdev.mentoria.lojavirtual.repository.AcessoRepository;
+import jdev.mentoria.lojavirtual.service.AcessoService;
+
+@SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
 class LojaVirtualMentoriaApplicationTests {
+	
+	@Autowired
+	private AcessoService acessoService;
+	
+	//@Autowired
+	//private AcessoRepository acessoRepository;
+	
+	@Autowired
+	private AcessoController acessoController;
 
 	@Test
-	void contextLoads() {
+	public void testCadastraAcesso() {
+		
+		Acesso acesso = new Acesso();
+		
+		acesso.setDescricao("ROLE_ADMIN");
+		
+		acessoController.salvarAcesso(acesso);
+		
 	}
 
 }
