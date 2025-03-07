6bfa40c2f58482c63f4aaa4e4c39756f8c6d1696
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-03-07 15:22:09 -0300

    Alteração de todos import's jakarta para javax; Criação WebConfigSecurity uma classe spring secutity

--- src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
+++ src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
@@ -1,21 +1,31 @@
 package jdev.mentoria.lojavirtual.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import jdev.mentoria.lojavirtual.model.Acesso;
 import jdev.mentoria.lojavirtual.service.AcessoService;
 
 @Controller
+@RestController
 public class AcessoController {
 	
 	@Autowired
 	private AcessoService acessoService;
 	
-	@PostMapping("/salvarAcesso")
-	public Acesso salvarAcesso(Acesso acesso) {
-		return acessoService.save(acesso);
+	@ResponseBody /*Poder dar um retorno da API*/
+	@PostMapping(value = "**/salvarAcesso") /*Mapeando a url para receber JSON*/
+	public ResponseEntity<Acesso> salvarAcesso(@RequestBody Acesso acesso) { /*Recebe o JSON e converte pra Objeto*/
+		
+		Acesso acessoSalvo = acessoService.save(acesso);
+		
+		return new ResponseEntity<Acesso> (acessoSalvo, HttpStatus.OK);
 	}
 
 }
