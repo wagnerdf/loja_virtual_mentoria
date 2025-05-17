3047ba51ee2e2a00d78df22721b2837dbddee12f
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-05-16 21:49:00 -0300

    CRUD Conta Pagar

--- src/main/java/jdev/mentoria/lojavirtual/model/ContaPagar.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/ContaPagar.java
@@ -1,9 +1,9 @@
 package jdev.mentoria.lojavirtual.model;
 
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.ConstraintMode;
@@ -20,66 +20,61 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 import jdev.mentoria.lojavirtual.enums.StatusContaPagar;
 
-@Entity 
+@Entity
 @Table(name = "conta_pagar")
-@SequenceGenerator(name = "seq_cconta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
 public class ContaPagar implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private String descricao;
-	
+
 	@Column(nullable = false)
 	private BigDecimal valorTotal;
-	
+
 	private BigDecimal valorDesconto;
-	
-	
-	@Enumerated(EnumType.STRING)
+
 	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
 	private StatusContaPagar status;
-	
+
 	@Column(nullable = false)
 	@Temporal(TemporalType.DATE)
 	private Date dtVencimento;
-	
+
 	@Temporal(TemporalType.DATE)
 	private Date dtPagamento;
-	
 
-	
-	@ManyToOne(targetEntity = Pessoa.class) /**Muitas contas a receber para uma pessoa : * to 1 **/
-	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk")) 
-	private Pessoa pessoa;
-	
-	@ManyToOne(targetEntity = Pessoa.class) /**Muitas contas a receber para uma pessoa : * to 1 **/
-	@JoinColumn(name = "pessoa_forn_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk")) 
-	private Pessoa pessoa_fornecedor;
-	
-	@ManyToOne(targetEntity = Pessoa.class)
-	@JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-	private Pessoa empresa;
+	@ManyToOne(targetEntity = PessoaFisica.class)
+	@JoinColumn(name = "pessoa_id", nullable = false,
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+	private PessoaFisica pessoaFisica;
 
-	public Pessoa getEmpresa() {
+	@ManyToOne(targetEntity = PessoaJuridica.class)
+	@JoinColumn(name = "pessoa_forn_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
+	private PessoaJuridica pessoa_fornecedor;
+	
+	@ManyToOne(targetEntity = PessoaJuridica.class)
+	@JoinColumn(name = "empresa_id", nullable = false, 
+	foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
+	private PessoaJuridica empresa;
+	
+	
+	public PessoaJuridica getEmpresa() {
 		return empresa;
 	}
 
-	public void setEmpresa(Pessoa empresa) {
+	public void setEmpresa(PessoaJuridica empresa) {
 		this.empresa = empresa;
-	}
-
-	public Pessoa getPessoa_fornecedor() {
-		return pessoa_fornecedor;
-	}
-
-	public void setPessoa_fornecedor(Pessoa pessoa_fornecedor) {
-		this.pessoa_fornecedor = pessoa_fornecedor;
 	}
 
 	public Long getId() {
@@ -104,6 +99,14 @@ public class ContaPagar implements Serializable {
 
 	public void setStatus(StatusContaPagar status) {
 		this.status = status;
+	}
+
+	public Pessoa getPessoa_fornecedor() {
+		return pessoa_fornecedor;
+	}
+
+	public void setPessoa_fornecedor(PessoaJuridica pessoa_fornecedor) {
+		this.pessoa_fornecedor = pessoa_fornecedor;
 	}
 
 	public Date getDtVencimento() {
@@ -138,17 +141,20 @@ public class ContaPagar implements Serializable {
 		this.valorDesconto = valorDesconto;
 	}
 
-	public Pessoa getPessoa() {
-		return pessoa;
+	public void setPessoaFisica(PessoaFisica pessoaFisica) {
+		this.pessoaFisica = pessoaFisica;
 	}
-
-	public void setPessoa(Pessoa pessoa) {
-		this.pessoa = pessoa;
+	
+	public PessoaFisica getPessoaFisica() {
+		return pessoaFisica;
 	}
-
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -160,6 +166,11 @@ public class ContaPagar implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		ContaPagar other = (ContaPagar) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
