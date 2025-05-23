975b51ebcb5ff18d38ccb5f4dadb2eac325f0dbf
Wagner Andrade <Wagnerandrade_df@hotmail.com>
2025-05-23 16:14:54 -0300

    Criando end-point de imagem de produto

---
+++ src/main/java/jdev/mentoria/lojavirtual/repository/ImagemProdutoRepository.java
@@ -0,0 +1,26 @@
+package jdev.mentoria.lojavirtual.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import jdev.mentoria.lojavirtual.model.ImagemProduto;
+
+@Repository
+@Transactional
+public interface ImagemProdutoRepository extends JpaRepository<ImagemProduto, Long> {
+
+	@Query("SELECT a FROM ImagemProduto a WHERE a.produto.id = ?1")
+	List<ImagemProduto> buscaImagemProduto(Long idProduto);
+	
+	@Transactional
+	@Modifying(flushAutomatically = true)
+	@Query(nativeQuery = true, value = "delete from imagem_produto where produto_id = ?1")
+	void deleteImagem(Long idProduto);
+
+
+}
